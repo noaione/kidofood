10ade6272c738eb1b451a7e1f3809b1d6c1cacaf
noaione <noaione0809@gmail.com>
2022-11-18 17:12:37 +0800

    backend: rewrite lint program

--- backend/pipelines/multi-lint.py
+++ backend/pipelines/multi-lint.py
@@ -1,18 +1,37 @@
 import subprocess as sp
+import sys
 from pathlib import Path
+from typing import Optional
 
 to_be_linted = ["internals", "routes", "app.py"]
 
-current_path = Path(__file__).absolute().parent.parent
-print(f"[*] Running tests at {current_path}")
-print("[*] Running safety test...")
-safety_res = sp.Popen(["safety", "check", "--full-report"]).wait()
-print("[*] Running isort test...")
-isort_res = sp.Popen(["isort", "-c"] + to_be_linted).wait()
-print("[*] Running flake8 test...")
-flake8_res = sp.Popen(["flake8", "--statistics", "--show-source", "--benchmark", "--tee"] + to_be_linted).wait()
+current_path = Path(__file__).absolute().parent.parent  # root dir
+venv_dir = [
+    current_path / ".venv",
+    current_path / "venv",
+    current_path / "env",
+]
+selected_venv_dir: Optional[Path] = None
+for venv in venv_dir:
+    if venv.exists():
+        selected_venv_dir = venv
 
-results = [(safety_res, "safety"), (isort_res, "isort"), (flake8_res, "flake8")]
+if selected_venv_dir is None:
+    raise RuntimeError("No virtual environment found")
+
+
+script_path = selected_venv_dir / "Scripts" if sys.platform == "win32" else selected_venv_dir / "bin"
+
+print(f"[*] Running tests at {current_path}")
+
+print("[*] Running isort test...")
+isort_res = sp.Popen([script_path / "isort", "-c"] + to_be_linted).wait()
+print("[*] Running flake8 test...")
+flake8_res = sp.Popen(
+    [script_path / "flake8", "--statistics", "--show-source", "--benchmark", "--tee"] + to_be_linted
+).wait()
+
+results = [(isort_res, "isort"), (flake8_res, "flake8")]
 any_error = False
 
 for res in results:
